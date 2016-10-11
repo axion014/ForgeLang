235398529273e1eedfe5d8e8081273986d076e3e
axion014 <axion014@gmail.com>
2016-10-11 16:10:55 +0900

    update version

--- src/io/github/axion014/forgelang/package-info.java
+++ src/io/github/axion014/forgelang/package-info.java
@@ -1,13 +1,13 @@
 /**
- * Java製コンパイラからexe, jar, js等が出力できるプログラミング言語。
+ * Java製コンパイラからexe, jar, js, py, rb等が出力できるプログラミング言語。
  * 逆にプログラム中でC, Java, Javascript, Python, Rubyなどのライブラリが使用できる。
- * 例えば.lib, .dll, .jar, .js, .pyなど。
+ * 例えば.lib, .dll, .jar, .js, .py, .rbなど。
  * もちろんWin32APIなども叩ける。
  * 世の中にある特徴もニーズも様々なプログラミング言語を統合する。
  * …ちょっと無理があるか?
  *
  * @author axion014
  * @since 2016/7/19
- * @version 0.1.2
+ * @version 0.1.3
  */
 package io.github.axion014.forgelang;
